7658cc15fd1dc9536fa668292de05d19224f86ec
Masaya ITO <masaya.ito@univ-rennes1.fr>
2020-05-01 17:03:52 +0900

    add test_masa.py

---
+++ test_masa.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8
+import numpy as np
+ 
+for i in [0, 1, 2]:
+    if i == 2:
+        print "Hello, Masaya!!"
+    elif i == 1:
+        print "See you !!"
+    else :
+        print "..."
